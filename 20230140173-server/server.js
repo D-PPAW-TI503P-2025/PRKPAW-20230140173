7d5dda6e9ab03558574d91a0f5024a16ab9c318a
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-11-06 16:12:25 +0700

    pertemuan 6

--- 20230140173-server/server.js
+++ 20230140173-server/server.js
@@ -8,6 +8,8 @@
 	const presensiRoutes = require("./routes/presensi");
 	const reportRoutes = require("./routes/reports");
 
+	const authRoutes = require('./routes/auth');
+
  	// Middleware
  	app.use(cors()); 
  	app.use(express.json()); 
@@ -25,6 +27,8 @@
 	app.use("/api/books", ruteBuku);
 	app.use("/api/presensi", presensiRoutes);
 	app.use("/api/reports", reportRoutes);
+	app.use('/api/auth', authRoutes);
+
 	app.listen(PORT, () => {
 	console.log(`Express server running at http://localhost:${PORT}/`);
 	});
---
+++ Tugas/Tugas6.md
@@ -0,0 +1,4 @@
+![Request POST /register (untuk membuat user "mahasiswa")](<ss/register paw mahasiswa.png>)
+![Request POST /register (untuk membuat user "admin").](<ss/register admin.png>)
+![Request POST /login (login sebagai "mahasiswa" dan mendapatkan token).](<ss/login mahasiswa.png>)
+![Database table User](<ss/database PAW.png>)
